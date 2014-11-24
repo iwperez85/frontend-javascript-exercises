eeca3846e545bc79ea6362e299f5a0d3013cb7d0
iwperez85 <iwperez85@yahoo.com>
2014-11-24 10:32:13 -0500

    loops over object done

--- 10-loops/01-iterating-over-an-object/challenge.js
+++ 10-loops/01-iterating-over-an-object/challenge.js
@@ -1,5 +1,22 @@
-module.exports.getKeys = undefined;
+module.exports.getKeys = function(object) {
+  output = [];
+  for (var k in object) {
+    output.push(k);
+  }
+  return output;
+}
+module.exports.getValues = function(object) {
+  output = [];
+  for (var k in object) {
+    output.push(object[k]);
+  }
+  return output;
+}
 
-module.exports.getValues = undefined;
-
-module.exports.objectToArray = undefined;
+module.exports.objectToArray = function(object) {
+  output = [];
+  for (k in object) {
+    output.push(k + " is " + object[k]);
+  }
+  return output;
+};
