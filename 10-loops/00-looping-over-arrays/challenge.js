1bb0ae9f5a46b91253acc4c6148f5b770d0ae556
iwperez85 <iwperez85@yahoo.com>
2014-11-24 10:11:25 -0500

    loop over array

--- 10-loops/00-looping-over-arrays/challenge.js
+++ 10-loops/00-looping-over-arrays/challenge.js
@@ -1,5 +1,24 @@
-module.exports.sumNumbers = undefined;
 
-module.exports.splitAndLowerCaseString = undefined;
+module.exports.sumNumbers = function(numbers) {
+  var sum = 0;
+  for (var i = 0; i < numbers.length; i++) {
+    sum += numbers[i];
+  }
+  return sum;
+};
 
-module.exports.addIndex = undefined;
+module.exports.splitAndLowerCaseString = function(inputString) {
+  var output = inputString.split(',');
+  for (var i = 0; i < output.length; i++) {
+    output[i] = output[i].toLowerCase();
+  }
+  return output;
+};
+
+module.exports.addIndex = function(arrayOfData) {
+  var output = [];
+  for (var i = 0; i < arrayOfData.length; i++) {
+    output.push(i + " is " + arrayOfData[i]);
+  }
+  return output;
+};
